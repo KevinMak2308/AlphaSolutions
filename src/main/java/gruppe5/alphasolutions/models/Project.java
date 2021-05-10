fa6a37bf6c00fe014d093b6e78b48c9099502dec
OliverChrestensen <oliver.chrestensen@hotmail.com>
2021-05-10 11:21:20 +0200

    project model

--- src/main/java/gruppe5/alphasolutions/models/Project.java
+++ src/main/java/gruppe5/alphasolutions/models/Project.java
@@ -1,4 +1,13 @@
 package gruppe5.alphasolutions.models;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashMap;
+
 public class Project {
+    private String title;
+    private String description;
+    private LocalDate StartDate;
+    private LocalDate Deadline;
+    private HashMap Tasks;
 }
