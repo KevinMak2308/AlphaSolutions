b07cd6f34fc0fdf5cdc928f71c930c1f55ad0a42
SePe19 <72380247+SePe19@users.noreply.github.com>
2021-05-10 13:20:03 +0200

    HTML

--- src/main/java/gruppe5/alphasolutions/services/Calculater.java
+++ src/main/java/gruppe5/alphasolutions/services/Calculater.java
@@ -1,4 +1,18 @@
 package gruppe5.alphasolutions.services;
 
+import gruppe5.alphasolutions.models.Project;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Calculater {
+    public void calc(){
+        LocalDate date = LocalDate.parse("2020-03-11");
+        System.out.print("Date : "+ date);
+        // Formatting Date
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/YYYY");
+        String dateLocal = formatter.format(date);
+        System.out.println("Date2 : " + dateLocal);
+    }
+
 }
